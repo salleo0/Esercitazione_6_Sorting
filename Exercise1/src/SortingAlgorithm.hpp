c545653c065a26f2d257e36b0bb221ddb9894631
Leonardo Sala <s310598@studenti.polito.it>
2025-04-25 16:49:27 +0200

    SortingAlgorithm.hpp completato

--- Exercise1/src/SortingAlgorithm.hpp
+++ Exercise1/src/SortingAlgorithm.hpp
@@ -6,18 +6,50 @@
 
 namespace SortLibrary {
 
-
-
 template<typename T>
+concept Sortable = requires(T& t) {
+	{t < t} -> std::convertible_to<bool>;
+};
+
+template<Sortable T>
 void SelectionSort(std::vector<T>& v)
 {
-
+	const unsigned int n = v.size();
+	
+	for (unsigned int i = 0; i < n - 1; i++)
+	{
+		unsigned int min_index = i;
+		for unsigned int j = i + 1; j < n; j++)
+		{
+			if (v[j] < v[min_index])
+				min_index = j;
+		}
+		
+		// swap
+		T tmp = v[i];
+		v[i] = v[min_index];
+		v[min_index] = tmp;
+	}
 }
 
-template<typename T>
+template<Sortable T>
 void InsertionSort(std::vector<T>& v)
 {
-
+	const unsigned int n = v.size();
+	
+	for (unsigned int i = 1; i < n; i++)
+	{
+		unsigned int j = i;
+		while(j > 0 && v[j] < v[j - 1])
+		{
+			// swap 
+			T tmp = v[j];
+			v[j] = v[j - 1];
+			v[j - 1] = tmp;
+			
+			j--;
+		}
+	}
 }
 
 }
