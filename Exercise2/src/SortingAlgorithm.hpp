7960c76bedb6cb95ee847a6f478fa5c3e030d10f
Leonardo Sala <s310598@studenti.polito.it>
2025-04-28 18:24:45 +0200

    Exercise2: algoritmo BubbleSort

--- Exercise2/src/SortingAlgorithm.hpp
+++ Exercise2/src/SortingAlgorithm.hpp
@@ -6,7 +6,26 @@
 using namespace std;
 
 namespace SortLibrary {
+template<typename T>
+concept Sortable = requires(T& t) {
+	{t < t} -> convertible_to<bool>;
+};
 
+template<Sortable T>
+void BubbleSort(vector<T>& v)
+{
+	const unsigned int n = v.size();
+	
+	for (unsigned int i = 0; i < n - 1; i++)
+		for (unsigned int j = i + 1; j < n; j++)
+			if (v[j] < v[i])
+			{
+				// swap 
+				T tmp = v[i];
+				v[i] = v[j];
+				v[j] = tmp;
+			}			
+}
 
 }
 
--- Exercise2/src_test/test_sorting_algorithm.hpp
+++ Exercise2/src_test/test_sorting_algorithm.hpp
@@ -8,14 +8,15 @@
 
 namespace SortLibrary {
 
-TEST(TestSorting, TestSelectionSort)
+TEST(TestSorting, TestBubbleSort)
 {
     std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
-    SelectionSort<int>(v);
+    BubbleSort<int>(v);
     std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
 }
 
+/*
 TEST(TestSorting, TestInsertionSort)
 {
     std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
@@ -23,5 +24,5 @@ TEST(TestSorting, TestInsertionSort)
     std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
 }
-
+*/
 }
