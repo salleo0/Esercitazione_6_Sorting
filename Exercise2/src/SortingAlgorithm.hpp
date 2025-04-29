416cd4dd495b5eda693354d9b359e89b0c163a24
Leonardo Sala <s310598@studenti.polito.it>
2025-04-29 02:15:09 +0200

    Exercise2: HeapSort completato

--- Exercise2/src/SortingAlgorithm.hpp
+++ Exercise2/src/SortingAlgorithm.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -19,14 +20,75 @@ void BubbleSort(vector<T>& v)
 	for (unsigned int i = 0; i < n - 1; i++)
 		for (unsigned int j = i + 1; j < n; j++)
 			if (v[j] < v[i])
+				swap(v[i], v[j]);
+}
+
+template<Sortable T>
+void HeapSort(vector<T>& v)
+{
+	const unsigned int n = v.size();
+
+	vector<T> heap;
+	heap.reserve(n);
+	
+	// 	ENQUEUE
+	for (const T& e : v)
+	{
+		heap.push_back(e);
+		unsigned int current_knot = heap.size()-1;
+		while(current_knot > 0)
+		{
+			unsigned int parent_knot = (current_knot - 1) / 2;
+			
+			if(heap[current_knot] > heap[parent_knot])
 			{
-				// swap 
-				T tmp = v[i];
-				v[i] = v[j];
-				v[j] = tmp;
-			}			
+				swap(heap[current_knot], heap[parent_knot]);
+				current_knot = parent_knot;
+			}
+			else
+				break;
+		}
+	}
+	
+	unsigned int heap_size = heap.size();
+	
+	// DEQUEUE
+	while(heap_size > 0)
+	{
+		v[heap_size-1] = heap[0];
+		swap(heap[0], heap[heap_size-1]);
+		heap_size--;
+		
+		unsigned int current_knot = 0;
+		while(true)
+		{
+			unsigned int right_knot = 2*current_knot + 2;
+			unsigned int left_knot = 2*current_knot + 1;
+			unsigned int largest_knot = current_knot;
+			
+			if(left_knot < heap_size && heap[left_knot] > heap[largest_knot])
+				largest_knot = left_knot;
+			
+			if(right_knot < heap_size && heap[right_knot] > heap[largest_knot])
+				largest_knot = right_knot;
+			
+			if (largest_knot != current_knot)
+			{
+				swap(heap[current_knot], heap[largest_knot]);
+				current_knot = largest_knot;
+			}
+			else
+				break;
+		}
+	}
 }
 
 }
 
-
+template<typename T>
+void swap(T& a, T& b) 
+{
+	T tmp = a;
+	a = b;
+	b = a;
+}
--- Exercise2/src_test/test_sorting_algorithm.hpp
+++ Exercise2/src_test/test_sorting_algorithm.hpp
@@ -16,13 +16,13 @@ TEST(TestSorting, TestBubbleSort)
     EXPECT_EQ(v, sortedV);
 }
 
-/*
-TEST(TestSorting, TestInsertionSort)
+
+TEST(TestSorting, TestHeapSort)
 {
     std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
-    InsertionSort<int>(v);
+    HeapSort<int>(v);
     std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
 }
-*/
+
 }
